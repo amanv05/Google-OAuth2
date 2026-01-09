7701b4da4f1ddbd2cc5c546c1a71376276b7f7fe
Aman <aman.ver9345@gmail.com>
2026-01-09 20:54:08 +0530

    added some changes to the files

--- frontend/src/components/GoogleLogin.tsx
+++ frontend/src/components/GoogleLogin.tsx
@@ -1,16 +1,35 @@
 import { useGoogleLogin } from "@react-oauth/google"
 import { googleAuth } from "../apiCall/api";
+import { useNavigate } from "react-router-dom";
+
+
+type GoogleAuthCodeResponse = {
+  code: string;
+  scope: string;
+};
+
 
 const GoogleLogin = () => {
 
+  const navigate = useNavigate();
+
+  const responseGoogle = async (authResult: GoogleAuthCodeResponse) => {
+    try {
+      const result = await googleAuth(authResult.code);
+        const { name, email, image } = result.data.user;
+        const token = result.data.token;
+        const obj = {name, email, image, token};
+        localStorage.setItem("user-info", JSON.stringify(obj));
+        navigate("/feed");
+    } catch (error) {
+      console.error("error occured while login", error);
+    }
+  }
+
 const googleLogin = useGoogleLogin({
-    onSuccess: async (authResult) => {
-        const result = await googleAuth(authResult.code);
-        const { name, email, image } = result.data;
-        console.log(result.data);
-    },
+    onSuccess: responseGoogle,
     onError: (e) => {
-      console.error("error occured while login", e);
+      console.error("error while loggin in", e);
     },
     flow: "auth-code"
   });
--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { googleLogout } from "@react-oauth/google";
+
+interface userInfo {
+  name: string,
+  email: string,
+  image: string,
+}
 
 
 const Feed = () => {
+
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState<userInfo | null>(null);
+
+  useEffect(() => {
+    const data = localStorage.getItem("user-info");
+    const userData = JSON.parse(data as string);
+    setUserInfo(userData);
+  }, []);
+
+  const handleLogout = () => {
+    googleLogout();
+    localStorage.removeItem("user-info");
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
+
   return (
     <div>
-      <h1>this is feed page</h1>
+      <h1>Welcome {userInfo?.name}</h1>
+      <h3>Email: {userInfo?.email}</h3>
+      <img src={userInfo?.image} alt="profile-img"></img>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
